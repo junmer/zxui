30c08de3c84de7f12d5bf10b950629e686dcfe3d
chris <wfsr@foxmail.com>
2013-06-08 23:55:36 +0800

    update doc

---
+++ README.md
@@ -0,0 +1,49 @@
+知心组件库 (zxui)
+=====================
+
+知心组件库是 ecom ui 1.1 规范的一个轻量级实现。依赖于 [Tangram 1.3.9](https://github.com/BaiduFE/Tangram-base) 和 [ESL](https://github.com/ecomfe/esl)
+
+[![Build Status](https://travis-ci.org/chriswong/zxui.png?branch=master)](https://travis-ci.org/chriswong/zxui)
+
+
+### 如何使用
+
+前期准备：
+
+git、nodejs、grunt-cli
+
+获取源码：
+
+	git clone git://github.com/chriswong/zxui.git
+	cd zxui
+
+安装依赖：
+
+	npm install
+
+生成文档：
+
+	grunt jsdoc
+
+单元测试：
+
+	grunt test
+
+代码覆盖率：
+
+	grunt cover
+
+查看可用任务：
+
+	grunt --help
+	
+
+### 目前实现的组件
+
+- 日历控件 [Calendar](http://chriswong.github.io/zxui/example/calendar.html)
+- 城市选择 [City](http://chriswong.github.io/zxui/example/city.html)
+- 分页控件 [Pager](http://chriswong.github.io/zxui/example/pager.html)
+- 浮层提示 [Tip](http://chriswong.github.io/zxui/example/tip.html)
+- 点击统计 [Log](http://chriswong.github.io/zxui/example/log.html)
+- 条件过滤 [Filter](http://chriswong.github.io/zxui/example/filter.html)
+- 下拉选项 [Select](http://chriswong.github.io/zxui/example/select.html)
---
+++ test/spec/FilterSpec.js
@@ -0,0 +1,219 @@
+    define(function (require) {
+        var Filter = require('Filter');
+        
+        var filter;
+
+        beforeEach(function () {
+            document.body.insertAdjacentHTML(
+                'beforeEnd', ''
+                    + '<div id="filterContainer" class="result-op"'
+                    + ' data-click="{x:1, srcid: 16874, p1:2, y:\'FD9FFD6C\'}"'
+                    + ' style="display:none">'
+                    +   '<form class="ecl-ui-filter" autocomplete="off">'
+                    +      '<p>按类型：'
+                    +      '    <label>'
+                    +      '        <input type="radio"'
+                    +      '            name="type" />全部'
+                    +      '    </label>'
+                    +      '    <label class="checked">'
+                    +      '        <input type="radio" name="type" value="1"'
+                    +      '            checked="checked" />角色扮演'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="radio"'
+                    +      '            name="type" value="2" />运动休闲'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="radio"'
+                    +      '            name="type" value="3" />射击游戏'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="radio"'
+                    +      '            name="type" value="4" />回合游戏'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="radio"'
+                    +      '            name="type" value="5" />策略经营'
+                    +      '    </label>'
+                    +      '</p>'
+                    +      '<p>按特色：'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="" />全部'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="1" />奇幻'
+                    +      '    </label>'
+                    +      '    <label class="checked">'
+                    +      '        <input type="checkbox" name="special"'
+                    +      '           value="2" checked="checked" />玄幻'
+                    +      '    </label>'
+                    +      '    <label class="checked">'
+                    +      '        <input type="checkbox" name="special"'
+                    +      '           value="3" checked="checked" />武侠'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="4" />历史'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="5" />写实'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="6" />魔幻'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="7" />体育'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="8" />科幻'
+                    +      '    </label>'
+                    +      '    <label>'
+                    +      '        <input type="checkbox"'
+                    +      '            name="special" value="9" />狂欢'
+                    +      '    </label>'
+                    +      '</p>'
+                    +   '</form>'
+                    + '</div>'
+            );
+
+            filter = new Filter({
+              prefix: 'ecl-ui-filter',
+              main: T.q('ecl-ui-filter')[0],
+              groups: 'p'
+            });
+            filter.render();
+            
+        });
+
+
+        afterEach(function () {
+            T.dom.remove(T.g('filterContainer'));
+            filter.dispose();
+        });
+      
+        describe('基本接口', function () {
+
+            it('控件类型', function () {
+
+                expect(filter.type).toBe('Filter');
+
+            });
+
+            it('getData', function () {
+                
+                expect(filter.getData('type'))
+                    .toEqual({key: 'type', value: ['1']});
+                
+                expect(filter.getData('special'))
+                    .toEqual({key: 'special', value: ['2', '3']});
+
+            });
+
+            it('disable & enable Items', function () {
+                
+                filter.disableItems('type', ['1', '2']);
+                var inputs = filter.groups.type.getElementsByTagName('input');
+
+                expect(inputs[1].checked).toBeFalsy();
+
+                filter.enableItems('type');
+                expect(inputs[2].parentNode.className)
+                    .not
+                    .toMatch(/\bdisabled\b/);
+
+            });
+
+            it('onClick - radio', function () {
+                var firedClick = false;
+                var firedChange = false;
+                var inputs = filter.groups.type.getElementsByTagName('input');
+                var target = inputs[3];
+                var event = {target: target};
+
+                var onClick = function (json) {
+                    firedClick = !firedClick;
+                    expect(json.event).toEqual(event);
+                };
+
+                var onChange = function (json) {
+                    firedChange = true;
+
+                    expect(json.key).toBe(target.name);
+                    expect(json.value[0]).toBe(target.value);
+                };
+
+
+                filter.on('click', onClick);
+                filter.on('change', onChange);
+
+                filter.onClick(event);
+                filter.onClick(event);
+
+                expect(firedClick).toBeFalsy();
+                expect(firedChange).toBeTruthy();
+
+                target = inputs[0];
+                event.target = target.parentNode;
+                filter.onClick(event);
+                filter.onClick(event);
+
+                filter.un('click', onClick);
+                filter.un('change', onChange);
+
+                expect(T.q('checked', target.parentNode.parentNode).length)
+                    .toBe(1);
+            });
+
+            it('onClick - checkbox', function () {
+                var inputs = filter.groups.special
+                    .getElementsByTagName('input');
+                var target = inputs[3];
+                var event = {target: target};
+                var changeCount = 0;
+
+                var onChange = function (json) {
+                    changeCount++;
+
+                    expect(json.key).toBe(target.name);
+
+                    T.each(json.value, function (v) {
+                        v && expect(inputs[v].checked).toBe(true);
+                    });
+                };
+
+
+                filter.on('change', onChange);
+                filter.onClick(event);
+                expect(changeCount).toBe(1);
+
+                target = inputs[0];
+                event.target = target.parentNode;
+
+                filter.onClick(event);
+                expect(changeCount).toBe(2);
+
+                filter.onClick(event);
+                expect(changeCount).toBe(2);
+
+                expect(T.q('checked', target.parentNode.parentNode).length)
+                    .toBe(1);
+
+                target = inputs[1];
+                event.target = target;
+                filter.onClick(event);
+
+                expect(changeCount).toBe(3);
+
+                filter.un('change', onChange);
+
+            });
+
+        });
+
+    });
